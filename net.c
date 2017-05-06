97d55d8e8c4cb2297efa42332f8b77699330b6ec
kevinleegithup <97213579@qq.com>
2017-05-06 18:05:08 +0800

    Update net.c

--- net.c
+++ net.c
@@ -65,6 +65,7 @@ make_server_socket(char *host, char *port)
         continue;
       }
 
+      /////设置为非阻塞 what?
       r = fcntl(fd, F_SETFL, flags | O_NONBLOCK);
       if (r == -1) {
         twarn("setting O_NONBLOCK");
@@ -72,6 +73,12 @@ make_server_socket(char *host, char *port)
         continue;
       }
 
+      /*
+      1、当有一个有相同本地地址和端口的socket1处于TIME_WAIT状态时，而你启动的程序的socket2要占用该地址和端口，你的程序就要用到该选项。 
+      2、SO_REUSEADDR允许同一port上启动同一服务器的多个实例(多个进程)。但每个实例绑定的IP地址是不能相同的。在有多块网卡或用IP Alias技术的机器可以测试这种情况。 
+      3、SO_REUSEADDR允许单个进程绑定相同的端口到多个socket上，但每个socket绑定的ip地址不同。这和2很相似，区别请看UNPv1。 
+      4、SO_REUSEADDR允许完全相同的地址和端口的重复绑定。但这只用于UDP的多播，不用于TCP。
+      */
       flags = 1;
       r = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof flags);
       if (r == -1) {
@@ -79,18 +86,32 @@ make_server_socket(char *host, char *port)
         close(fd);
         continue;
       }
+      /*
+      设置SO_KEEPALIVE选项来开启KEEPALIVE，然后通过TCP_KEEPIDLE、TCP_KEEPINTVL和TCP_KEEPCNT设置keepalive的开始时间、间隔、次数等参数。
+      当然，也可以通过设置/proc/sys/net/ipv4/tcp_keepalive_time、tcp_keepalive_intvl和tcp_keepalive_probes等内核参数来达到目的，但是这样的话，会影响所有的socket，因此建议使用setsockopt设置。
+      */
       r = setsockopt(fd, SOL_SOCKET, SO_KEEPALIVE, &flags, sizeof flags);
       if (r == -1) {
         twarn("setting SO_KEEPALIVE on fd %d", fd);
         close(fd);
         continue;
       }
+      /*
+      linger，顾名思义是延迟延缓的意思，这里是延缓面向连接的socket的close操作。
+      默认，close立即返回，但是当发送缓冲区中还有一部分数据的时候，系统将会尝试将数据发送给对端。SO_LINGER可以改变close的行为
+      这个选项需要谨慎使用，尤其是强制式关闭，会丢失服务器发给客户端的最后一部分数据。UNP中:
+The TIME_WAIT state is our friend and is there to help us(i.e., to let the old duplicate segments expire in the network).
+      */
       r = setsockopt(fd, SOL_SOCKET, SO_LINGER, &linger, sizeof linger);
       if (r == -1) {
         twarn("setting SO_LINGER on fd %d", fd);
         close(fd);
         continue;
       }
+      /*
+      快速发包
+      cork是大量发包
+      */
       r = setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &flags, sizeof flags);
       if (r == -1) {
         twarn("setting TCP_NODELAY on fd %d", fd);
